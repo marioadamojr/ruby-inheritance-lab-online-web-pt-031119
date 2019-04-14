b0641ba1ec4e2996f81d1494f2cc70f015931751
Mario Adamo Jr <marioadamojr@gmail.com>
2019-04-14 15:10:55 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,16 @@
-class Student 
+require_relative "./user.rb"
+require_relative "./teacher.rb"
+class Student < User
 
+  def initialize()
+    @knowledge = []
+  end
+
+  def learn(lesson)
+    @knowledge << lesson
+  end
+
+  def knowledge
+    @knowledge
+  end
 end
